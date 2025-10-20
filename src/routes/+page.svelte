bcd75b1cac38fdb4a5ef4e4603160bf37f0fa7ac
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-20 12:04:06 +0100

    initial commit (sveltekit-starter)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {
+    Header,
+    Section,
+    Footer,
+  } from "@onsvisual/svelte-components";
+</script>
+
+<Header compact title="Empty project" />
+
+<Section marginTop="{true}">
+  <p>
+    This is an empty project.
+  </p>
+</Section>
+
+<Footer compact />
