c1d96a0a2f18d2e5d147605501894ecd574a65f6
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-22 07:37:50 +0100

    fix paths in app config

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 // APP CONFIG
 
 // Base paths
-export const base_prod = '/visualisations/sveltekit-starter'; // Directory on the ONS website
-export const base_preview = '/sveltekit-starter'; // Directory on datavisweb preview server or Github Pages
+export const base_prod = '/dot-map-poc'; // Directory on the ONS website
+export const base_preview = '/dot-map-poc'; // Directory on datavisweb preview server or Github Pages
 
